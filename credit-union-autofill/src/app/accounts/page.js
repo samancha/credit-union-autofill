5b5c6c4d7c284b5060c29bd4a8d2b8ea6c2acaed
steve amancha <myc0mnsense@gmail.com>
2023-06-14 11:00:50 -0700

    created inital pages for devs to work on

---
+++ credit-union-autofill/src/app/accounts/page.js
@@ -0,0 +1,5 @@
+
+
+export default function Page() {
+    return <h1>Hello from the ACCOUNTS DIRECTORY</h1>
+  }
---
+++ credit-union-autofill/src/app/form/page.js
@@ -0,0 +1,5 @@
+
+
+export default function Page() {
+    return <h1>Hello from the FORM DIRECTORY</h1>
+  }
