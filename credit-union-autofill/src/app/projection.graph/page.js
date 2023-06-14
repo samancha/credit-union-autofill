62ab4c3065ec964ba6c93bcf0564d4761fa6aadd
sf-tony-welcome <tony.welcome.vabzto@statefarm.com>
2023-06-14 14:34:24 -0700

    marc changes + json data file at root

--- credit-union-autofill/src/app/projection.graph/page.js
+++ credit-union-autofill/src/app/projection.graph/page.js
@@ -1,5 +1,69 @@
 
 
 export default function Page() {
-    return <h1>Hello from the PROJECTION.GRAPH DIRECTORY</h1>
-  }
+
+const DATA_COUNT = 5;
+const NUMBER_CFG = {count: DATA_COUNT, min: -100, max: 100};
+
+
+const labels = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July'
+]
+
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: months(NUMBER_CFG),
+      borderColor: Utils.CHART_COLORS.red,
+      backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
+    },
+    {
+      label: 'Dataset 2',
+      data: Utils.months(NUMBER_CFG),
+      borderColor: Utils.CHART_COLORS.blue,
+      backgroundColor: Utils.transparentize(Utils.CHART_COLORS.blue, 0.5),
+    }
+  ]
+};
+    }
+    ;
+    const config = {
+      type: 'line',
+      data: data,
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+            text: 'Chart.js Line Chart'
+          }
+    }
+  },
+    
+};
+  
+  
+
+  
+
+  // /return (
+  //   <div>
+  //     <h1>Hello from the PROJECTION.GRAPH DIRECTORY</h1>
+  //     <h1>Hello from the PROJECTION.GRAPH DIRECTORY</h1>
+  //     <div>{chart}</div>
+  //   </div>
+  // );
+
+  
+
