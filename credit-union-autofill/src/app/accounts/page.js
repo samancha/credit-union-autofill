b512980ed1c5495732b912019603ecbbfd7a0c95
Cmillma <cmillma23@gmail.com>
2023-06-14 15:56:29 -0700

    calculator

--- credit-union-autofill/src/app/accounts/page.js
+++ credit-union-autofill/src/app/accounts/page.js
@@ -1,5 +1,67 @@
+'use client'
+import React, {useState} from "react";
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
+
+// const Page = () => {
+//   const [savingsGoal, setSavingsGoal] = useState();
+//   const [frequency, setFrequency] = useState();
+//   const [contributions, setContributions] = useState();
+//   const [interest, setInterestRate] = useState();
+//   const [result, setResult] = useState();
+
+const calculate = () => {
+  // contribution = (Monthly contribution = FV / ((1 + r) ^ n - 1) / r
+// Where:
+// FV = Future value (desired amount) = $10,000
+// r = Monthly interest rate = 0.00417
+// n = Number of compounding periods = 60
+
+    const
+}
 
 export default function Page() {
-    return <h1>Hello from the ACCOUNTS DIRECTORY</h1>
-  }
+
+  return (
+  <div className="SavingCalculator">
+      <h1>Savings Calculator</h1>
+    <Stack 
+      direction='column'
+      spacing={2}
+    >
+    <form>
+      <TextField 
+        label="Total Savings Goal" 
+        variant="outlined" 
+        onChange={(e) => setSavingsGoal(e.target.value)}
+      />
+      <TextField 
+        label="Frequency" 
+        variant="outlined" 
+        onChange={(e) => setFrequency(e.target.value)}
+      />
+      <TextField 
+        label="Contributions" 
+        variant="outlined" 
+        onChange={(e) => setContributions(e.target.value)}
+      />
+      <TextField 
+        label="Annual Interest" 
+        variant="outlined"
+        onChange={(e) => setInterestRate(e.target.value / 100)}
+      />
+      <Button 
+        variant='contained'
+        color="primary"
+        onClick={() => {}}>
+          Calculate
+      </Button>
+    </form> 
+    </Stack>    
+  </div>
+  ) 
+  
+}
+
